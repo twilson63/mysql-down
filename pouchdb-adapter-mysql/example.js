5944d5c7fe8249b17af58136f3dfe6cf00e76244
Tom Wilson <tom@jackhq.com>
2018-01-20 07:52:57 -0500

    updated version to be set to compatible version of pouchdb

--- pouchdb-adapter-mysql/example.js
+++ pouchdb-adapter-mysql/example.js
@@ -5,7 +5,10 @@ const db = PouchDB('bar', {
   prefix: 'foo/'
 })
 
-db.put({ _id: 'boop3', hello: 'world' }).catch(err => console.log(err.message))
+// db
+//   .put({ _id: 'boop3', hello: 'world' })
+//   .then(res => console.log(res))
+//   .catch(err => console.log(err.message))
 db
   .allDocs({ include_docs: true })
   .then(res => console.log(JSON.stringify(res, null, 2)))
